89ea6dd95027c981690505b562c20af8819305ba
Angelo Pardo <matenji@gmail.com>
2015-12-02 22:27:12 -0600

    Bootstrapping meteor
    Created new jsx files for React; created "tasks" collection in mongo.

---
+++ App.jsx
@@ -0,0 +1,30 @@
+// App component - represents the whole app
+App = React.createClass({
+    mixins: [ReactMeteorData],
+
+    getMeteorData() {
+        return {
+            tasks: Tasks.find({}).fetch()
+        }
+    },
+
+    renderTasks() {
+        return this.data.tasks.map((task) => {
+            return <Task key={task._id} task={task} />;
+        });
+    },
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+                </header>
+
+                <ul>
+                    {this.renderTasks()}
+                </ul>
+            </div>
+        );
+    }
+});
